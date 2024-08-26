14cd22894c379f5df8b262d7d62d64df4dd426c7
Justin Hoang <j124.dev@gmail.com>
2024-08-25 20:44:35 -0600

    Added better obsidian, nvim-surround, and pomo Added ruler and line autowrap Removed `mini.surround` because it had weird shortcuts that weren't like the `vim` version

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.o.cc = '80,81'
+vim.o.textwidth = 80
+vim.o.conceallevel = 2
+
 return {
   -- Escape easier with jk
   vim.keymap.set('i', 'jk', '<Esc>'),
--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,19 +1,57 @@
 return {
   {
-    --
-    -- :RenderMarkdown | :RenderMarkdown enable - Enable this plugin
-    --     Can also be accessed directly through require('render-markdown').enable()
-    -- :RenderMarkdown disable - Disable this plugin
-    --     Can also be accessed directly through require('render-markdown').disable()
-    -- :RenderMarkdown toggle - Switch between enabling & disabling this plugin
-    --     Can also be accessed directly through require('render-markdown').toggle()
-    -- :RenderMarkdown expand - Increase anti-conceal margin above and below by 1
-    --     Can also be accessed directly through require('render-markdown').expand()
-    -- :RenderMarkdown contract - Decrease anti-conceal margin above and below by 1
-    --     Can also be accessed directly through require('render-markdown').contract()
-    --
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    -- Obsidian provides nice formatting
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/Documents/vaults/personal',
+        },
+        {
+          name = 'research',
+          path = '~/Documents/vaults/research',
+        },
+      },
+
+      notes_subdir = 'notes',
+
+      -- see below for full list of options 👇
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = 'dailies',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = '%Y-%m-%d',
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = '%B %-d, %Y',
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { 'daily-notes' },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
+
+      templates = {
+        folder = 'templates',
+        date_format = '%Y-%m-%d-%a',
+        time_format = '%H:%M',
+      },
+    },
   },
 }
