14df99270d1a459c6c9c9886d85724c8e6e83aa2
Justin Hoang <j124.dev@proton.me>
2025-09-05 10:16:57 -0600

    feat(plugins): added nvim-ufo folding plugin

---
+++ lua/custom/plugins/nvim-ufo.lua
@@ -0,0 +1,16 @@
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = { 'kevinhwang91/promise-async' },
+  lazy = false,
+  config = function()
+    -- Option 3: treesitter as a main provider instead
+    -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+    -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+    -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+    require('ufo').setup {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+      end,
+    }
+  end,
+}
