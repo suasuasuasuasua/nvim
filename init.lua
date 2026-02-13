c5a2b94c744b457863f38c58ec400db5bb830549
Justin Hoang <justinhoang@sua.dev>
2026-02-12 18:38:20 -0700

    build(mason): add mason-lspconfig

--- init.lua
+++ init.lua
@@ -481,6 +481,7 @@ require('lazy').setup({
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
+      { 'mason-org/mason-lspconfig.nvim', opts = {} },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
@@ -593,9 +594,9 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        clangd = {},
+        gopls = {},
+        pylsp = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -614,8 +615,11 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        -- 'lua_ls', -- Lua Language server
-        -- 'stylua', -- Used to format Lua code
+        'black',
+        'clang-format',
+        'isort',
+        'lua_ls', -- Lua Language server
+        'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
 
